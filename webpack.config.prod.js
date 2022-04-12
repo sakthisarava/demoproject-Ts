8f46f84ff521adbe4a38b4ec616a3c0ee3a7a32a
sakthi s <velsakthis5301@gmail.com>
2022-04-12 08:50:06 +0530

    first commit

---
+++ webpack.config.prod.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const cleanPlugin = require('clean-webpack-plugin');
+const { CleanPlugin } = require('webpack');
+
+module.exports = {
+    mode: 'production',
+    entry: './src/app.ts',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: 'dist'
+    },
+    devtool: 'none',
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                use: 'ts-loader',
+                exclude: /node_modules/
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.ts', '.js']
+    },
+    plugins: [
+        new cleanPlugin.CleanWebpackPlugin()
+    ]
+};
